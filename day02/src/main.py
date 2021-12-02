46a37886facc965a168ea104176a2e253f61e0fe
Anna Moshkina <a.v.moshkina@gmail.com>
2021-12-02 11:11:54 +0100

    Day 1, part 1

---
+++ day02/src/main.py
@@ -0,0 +1,29 @@
+class Submarine:
+
+    def __init__(self):
+        self.horizontal = 0
+        self.depth = 0
+
+    def forward(self, shift):
+        self.horizontal += shift
+
+    def down(self, shift):
+        self.depth += shift
+
+    def up(self, shift):
+        self.depth -= shift
+
+    def run(self):
+        with open("input.txt") as fd:
+            commands = [line.strip().split(" ") for line in fd.readlines()]
+
+            # print(getattr(self, 'forward')(4))
+            
+            for cmd, shift in commands:
+                getattr(self, cmd)(int(shift))
+
+
+if __name__ == '__main__':
+    submarine = Submarine()
+    submarine.run()
+    print("Day2, part 1: %s" % (submarine.horizontal * submarine.depth))
