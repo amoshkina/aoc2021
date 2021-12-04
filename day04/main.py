97e5ef41f04d8ae75abee16d7ece80f17eb0fdc6
Anna Moshkina <a.v.moshkina@gmail.com>
2021-12-04 21:08:30 +0100

    Day 4, part 1

---
+++ day04/main.py
@@ -0,0 +1,72 @@
+class Coord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.marked = False
+
+
+class Board:
+    def __init__(self, rows):
+        self.won = False
+        self.rows = [0] * 5
+        self.cols = [0] * 5
+
+        self.matrix = {}
+
+        for i, row in enumerate(rows):
+            for j, value in enumerate(row):
+                self.matrix[value] = Coord(i, j)
+
+
+class Solver:
+    def __init__(self):
+        self.numbers, self.boards = self.read_input()
+
+    @staticmethod
+    def read_input():
+        with open('input.txt') as fd:
+            data = fd.read()
+            data = data.split('\n\n')
+            numbers = data[0].split(',')
+            boards = set()
+            for idx, board in enumerate(data):
+                if idx == 0:
+                    continue
+
+                rows = map(lambda x: x.split(), board.split('\n'))
+                boards.add(Board(rows))
+
+            return numbers, boards
+
+    def play(self):
+        for number in self.numbers:
+            for board in self.boards:
+                if number not in board.matrix:
+                    continue
+
+                coord = board.matrix[number]
+                coord.marked = True
+                board.rows[coord.x] += 1
+                board.cols[coord.y] += 1
+
+                if board.rows[coord.x] == 5 or board.cols[coord.y] == 5:
+                    return board, int(number)
+
+    @staticmethod
+    def score(board, number):
+        umarked_sum = 0
+        for value, coord in board.matrix.items():
+            if not coord.marked:
+                umarked_sum += int(value)
+
+        return umarked_sum * number
+
+    def part1(self):
+        return self.score(*self.play())
+
+
+if __name__ == '__main__':
+    solver = Solver()
+
+    print('Day 4, part 1: ', solver.part1())
+
