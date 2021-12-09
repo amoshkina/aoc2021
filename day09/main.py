c750c3dd2898753d3e52e7ab5a60efff6e63cc9d
Anna Moshkina <a.v.moshkina@gmail.com>
2021-12-09 21:45:30 +0100

    Day 9, part 2

--- day09/main.py
+++ day09/main.py
@@ -1,9 +1,13 @@
+from collections import defaultdict
+
+
 class Solver:
 
     def __init__(self):
         self.matrix = self.read_input()
         self.max_row = len(self.matrix)
         self.max_col = len(self.matrix[0])
+        self.component_size = defaultdict(int)
 
     @staticmethod
     def read_input():
@@ -25,7 +29,13 @@ class Solver:
 
                 local_min = True
                 for i1, j1 in neighbours:
-                    if 0 <= i1 < self.max_row and 0 <= j1 < self.max_col and self.matrix[i1][j1] <= height:
+                    cond = (
+                        0 <= i1 < self.max_row and
+                        0 <= j1 < self.max_col and
+                        self.matrix[i1][j1] <= height
+                    )
+
+                    if cond:
                         local_min = False
                         break
 
@@ -34,6 +44,41 @@ class Solver:
 
         return result
 
+    def bfs(self, i, j, component):
+        neighbours = [
+            (i-1, j), (i+1, j),
+            (i, j-1), (i, j+1)
+        ]
+
+        for i1, j1 in neighbours:
+            cond = (
+                0 <= i1 < self.max_row and
+                0 <= j1 < self.max_col and
+                0 <= self.matrix[i1][j1] < 9
+            )
+
+            if cond:
+                self.matrix[i1][j1] = component
+                self.component_size[component] += 1
+                self.bfs(i1, j1, component)
+
+    def score(self):
+        sorted_sizes = sorted(self.component_size.values(), reverse=True)
+        assert len(sorted_sizes) >= 3
+        return sorted_sizes[0] * sorted_sizes[1] * sorted_sizes[2]
+
+    def part2(self):
+        component = -1
+        for i, row in enumerate(self.matrix):
+            for j, height in enumerate(row):
+                if height < 0 or height == 9:
+                    continue
+                self.bfs(i, j, component)
+                component -= 1
+
+        return self.score()
+
 
 if __name__ == '__main__':
     print('Day 9, part 1: ', Solver().part1())
+    print('Day 9, part 2: ', Solver().part2())
